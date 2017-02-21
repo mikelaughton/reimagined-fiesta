df0c43a6920e1e4b64101a16236ca5a9e68c64a9
mikelaughton <michael.laughton@gmail.com>
2017-02-21 22:46:41 +0000

    Added login requirement. Still views missing though.

--- reminders/views.py
+++ reminders/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-
+from django.contrib.auth import authenticate, login
+#For @login_required
+from django.contrib.auth.decorators import login_required
+#Because you can't decorate classes...
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 from .models import *
 
+@method_decorator(login_required,name='dispatch')
 class IndexView(generic.ListView):
 	template_name = 'reminders/index.html'
 	context_object_name = 'tasks'
