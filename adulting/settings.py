26afbff16bce42e36d83d873c8fa1a82d2c07902
Mike Laughton <michael.laughton@gmail.com>
2017-02-28 22:38:22 +0000

    Added favicon HTML.

--- adulting/settings.py
+++ adulting/settings.py
@@ -15,18 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l5of6a$o*uh5n2utu7m)e022@08%bhez8o@2ak2uzolz0$g__%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['pi-desktop','192.168.0.20','192.168.0.10','localhost']
-
+from .local_settings import *
 
 # Application definition
 
@@ -70,32 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'adulting.wsgi.application'
 
-
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-if not DEBUG:
-	DATABASES = {
-
-		'default': {
-				'ENGINE': 'django.db.backends.mysql',
-				'NAME': 'adulting_database',
-				'USER': 'adulter',
-				'PASSWORD':'Vladonia1',
-				'HOST':'localhost',
-				'PORT':'',
-			}
-	
-	}
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -132,10 +95,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),'/var/www/adulting/static']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
-if DEBUG == False:
-	STATIC_ROOT = '/var/www/adulting/static'
-	MEDIA_ROOT = '/var/www/adulting/media'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
